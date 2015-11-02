27044598db595841e5d6733b3006c3a4ea665eb5
Nick Orton <no@knewton.com>
2015-11-02 15:25:51 -0500

    Adding problem 3

--- src/Main.hs
+++ src/Main.hs
@@ -2,9 +2,11 @@ module Main where
 
 import qualified Problem001 as P1
 import qualified Problem002 as P2
+import qualified Problem003 as P3
 
 --main = putStrLn "The answer to problem one is " ++ show p1.answer
 main = putStrLn 
        (   "Problem 1: " ++ (show P1.answer) ++ "\n"
-        ++ "Problem 2: " ++ (show P2.answer)
+        ++ "Problem 2: " ++ (show P2.answer) ++ "\n"
+        ++ "Problem 3: " ++ (show P3.answer)
        )
--- src/Problem003.hs
+++ src/Problem003.hs
@@ -1,3 +1,6 @@
 module Problem003 where
 
+import Math.NumberTheory.Primes.Factorisation
 
+primes = map (\(x,_) -> x) (factorise 600851475143)
+answer = foldl max 0 primes
